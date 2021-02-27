09302a47214c23bdea71975140ee1be099208b37
Pavel_Freeman <akik_2018@mail.ru>
2021-02-27 04:03:43 +0300

    Code of the game(Remake 2.0) *************************** Fixed name of variables

--- game.py
+++ game.py
@@ -5,75 +5,75 @@ logging.basicConfig(level=logging.INFO)
 def main():
     logging.info("Вас приветствует игра GuessTheNumber. Она загадает число от 1 до 100, а вы должны его отгадать!")
     
-    mag = randint(1, 100)
-    user = 0
+    magic_number = randint(1, 100)
+    user_number = 0
     Attempt = 0
-    xod = 1
+    step = 1
     
-    pla = int(input("Введите количество игроков (от 1 до 2) : "))
+    players = int(input("Введите количество игроков (от 1 до 2) : "))
     
-    if pla == 1 :
-        while user != mag :
-            user = int(input("Ваше число? : "))
-            if user < mag :
+    if players == 1 :
+        while user_number  != magic_number :
+            user_number  = int(input("Ваше число? : "))
+            if user_number  < magic_number :
                 logging.info('Ваше число меньше, чем магическое.')
                 Attempt += 1
-            elif user > mag :
+            elif user_number  > magic_number :
                 logging.info("Ваше число больше, чем магическое.")
                 Attempt += 1
-            elif user > 100 or user < 1:
+            elif user_number  > 100 or user_number  < 1:
                 logging.info("Ваше число не подходит в диапазон")
     
-    if pla == 2 :
-        user2 = 0
-        while user != mag or user2 != mag:
-            user = int(input("Ходит игрок 1. Ваше число? : "))
-            if user < mag :
+    if players == 2 :
+        user_number2 = 0
+        while user_number  != magic_number or user_number2 != magic_number:
+            user_number = int(input("Ходит игрок 1. Ваше число? : "))
+            if user_number < magic_number :
                 logging.info("Ваше число меньше, чем магическое.")
                 Attempt += 1
-                xod = 2
-            if user > mag :
+                step = 2
+            if user_number > magic_number :
                 logging.info("Ваше число больше, чем магическое.")
                 Attempt += 1
-                xod = 2
-            if user > 100 or user < 1:
+                step = 2
+            if user_number > 100 or user_number < 1:
                 logging.info("Ваше число не подходит в диапазон")
-                xod = 2
+                step = 2
     
-            if user==mag:
+            if user_number==magic_number:
                 break
     
-            user2 = int(input("Ходит игрок 2. Ваше число? : "))
-            if user2 < mag :
+            user_number2 = int(input("Ходит игрок 2. Ваше число? : "))
+            if user_number2 < magic_number :
                 logging.info("Ваше число меньше, чем магическое.")
                 Attempt += 1
-                xod = 1
-            if user2 > mag:
+                step = 1
+            if user_number2 > magic_number:
                 logging.info("Ваше число больше, чем магическое.")
                 Attempt += 1
-                xod = 1
-            if user2 > 100 or user2 < 1:
+                step = 1
+            if user_number2 > 100 or user_number2 < 1:
                 logging.info("Ваше число не подходит в диапазон")
-                xod = 1
+                step = 1
     
-            if user2==mag:
+            if user_number2==magic_number:
                 break
     
             
     
     Attempt += 1
-    if pla == 1 :
+    if  players == 1 :
         logging.info("Ура! Вы отгадали число!")
-        logging.info("Правильный ответ - " + str(mag))
+        logging.info("Правильный ответ - " + str(magic_number))
         logging.info("Вы угадали это число с " + str(Attempt) + "-й попытки.")
-    if pla == 2:
-        if xod == 1 :
+    if players == 2:
+        if step == 1 :
             logging.info("Поздравляю, игрок 1! Вы выиграли!")
-            logging.info("Правильный ответ - " + str(mag))
+            logging.info("Правильный ответ - " + str(magic_number))
             logging.info("Вы угадали это число с " + str(Attempt) + "-й попытки.")
-        elif xod == 2 :
+        elif step == 2 :
             logging.info("Поздравляю, игрок 2! Вы выиграли!")
-            logging.info("Правильный ответ - " + str(mag))
+            logging.info("Правильный ответ - " + str(magic_number))
             logging.info("Вы угадали это число с " + str(Attempt) + "-й попытки.")
 if __name__ == '__main__':            
         main()
